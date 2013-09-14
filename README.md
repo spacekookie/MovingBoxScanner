9c3515255fff9075458dea069185fa6b4920a161
Katharina Sabel <sabel.katharina@gmail.com>
2013-09-14 18:29:24 +0200

    Added some stuff

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # About this Application
+
+The Box Inventory Scanner (also Box Scan Pro) is an application that accesses your local QR code scanner, reads a QR code and then displays the stored information in an organised fashion.
+
+The application was designed to work in cooperation with our web applet over at www.2rsoftworks.de/moveit
+
+Alternatively you can just create your own xml texts in this manner:
+
+<box>
+	<name>Name</name>
+	<location>Location</location>
+	<contents>
+		<item>Item 1</item>
+		<item>Item 2</item>
+		<item>Item 3</item>
+	</contents>
+</box>
+
+ONLY STORE ONE BOX PER QR CODE. Additional boxes will be ignored!
--- gen/de/r2soft/movescanpro/R.java
+++ gen/de/r2soft/movescanpro/R.java
@@ -27,7 +27,7 @@ public final class R {
         public static final int LinearLayout1=0x7f080000;
         public static final int TableLayout1=0x7f08000a;
         public static final int action_credits=0x7f08000c;
-        public static final int display_header=0x7f080004;
+        public static final int display_header=0x7f080005;
         public static final int display_name=0x7f080002;
         public static final int display_room=0x7f080003;
         public static final int home_button_scan=0x7f080009;
@@ -36,7 +36,7 @@ public final class R {
         public static final int item_list_label=0x7f08000b;
         public static final int tableRow1=0x7f080001;
         public static final int textView1=0x7f080007;
-        public static final int welcome_screen_fragment_container=0x7f080005;
+        public static final int welcome_screen_fragment_container=0x7f080004;
     }
     public static final class layout {
         public static final int activity_display=0x7f030000;
